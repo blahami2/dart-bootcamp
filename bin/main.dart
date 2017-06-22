d00fb8751d98fc11572b2cdea56028bbc7a25840
Michael Bláha <blahami2@gmail.com>
2017-06-22 09:02:36 +0200

    Some minor tests

--- bin/main.dart
+++ bin/main.dart
@@ -4,5 +4,16 @@
 import 'package:DartAcademyBootCamp/DartAcademyBootCamp.dart' as DartAcademyBootCamp;
 
 main(List<String> arguments) {
+  List<int> numbers = [1, 2, 3, 4, 5, 6];
+  Map<int, int> map = {1: 1, 2: 2, 3: 3};
+  print(numbers);
+  print(map);
+
   print('Hello world: ${DartAcademyBootCamp.calculate()}!');
+  int x = 6;
+  x += 12;
+  print("The result is $x");
+  print("Alternative result is ${1/5}");
+  print("Some recursion of print: ${print("printing print inside print")}");
+  print(5 / 3);
 }
