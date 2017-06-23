1405b00bb5ff10729df07918fef27f971ff522a6
Michael Bláha <blahami2@gmail.com>
2017-06-23 09:03:53 +0200

    Finished the basics

--- bin/main.dart
+++ bin/main.dart
@@ -3,8 +3,8 @@
 
 import 'package:DartAcademyBootCamp/DartAcademyBootCamp.dart' as DartAcademyBootCamp;
 
-main(List<String> arguments) {
-  // types
+void main(List<String> arguments) {
+  print("************************ TYPES ************************");
   var a = 6 / 5;
   int b = (6 / 5).floor(); // everything is object
   num b2 = 6 / 5; // decimal or integer
@@ -20,18 +20,18 @@ main(List<String> arguments) {
     3: 3
   }; // LinkedHashMap as the default implementation
   print("$a $b $b2 $b3 $b4 $c $numbers $map $b5");
-  // operators
+  print("************************ OPERATORS ************************");
   int x = 6;
   x += 12;
   x++;
   print("${++x} = 20");
-  // String
+  print("************************ STRING ************************");
   String d = "text";
   String e = '"text"';
   String f = r'\text';
   String g = "text";
   print("$d $e $f ${g.toUpperCase()[2]}"); // ${} interpolated expression
-  // conditions and boolean operators
+  print("************************ CONDITIONS AND BOOLEAN OPERATORS ************************");
   // pretty much the same as in Java
   if ("text" == 'text') {
     print("equal!");
@@ -42,21 +42,35 @@ main(List<String> arguments) {
   }
   var h = "text";
   var m = "text";
-  switch(h){
-    //case m: must be constant
+  switch (h) {
+  //case m: must be constant
     case "text":
     case "other_text":
       print("$h is either text or other_text");
       break;
     default:
-      // default stuff
+    // default stuff
   }
-  // Modulo
-  print("${5 % 3} ${0 % 3} ${-5 % 3}"); // works properly for negative numbers, cool
-  // other
+  print("************************ MODULO ************************");
+  print("${5 % 3} ${0 % 3} ${-5 % 3}"); // works properly for negative numbers
+  print("************************ LOOPS ************************");
+  while (a < 3) {
+    print("${a++}");
+  }
+  do { // do at least once
+    print("${a++}");
+  } while (a < 5);
+  for(num i = 0; i < 2; i++){
+    print("$i");
+  }
+  print("************************ OTHER ************************");
   print('Hello world: ${DartAcademyBootCamp.calculate()}!');
   print("The result is $x");
   print("Alternative result is ${1 / 5}");
   print("Some recursion of print: ${print("printing print inside print")}");
   print(5 / 3);
+}
+
+void printHeader(String header){
+  print("************************ $header ************************");
 }
