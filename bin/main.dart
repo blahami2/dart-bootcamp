75adc6471500e22a1cb3e53d513835eb0f651838
Michael Bláha <blahami2@gmail.com>
2017-06-26 14:49:57 +0200

    Added some more language features, including pub file change

--- bin/main.dart
+++ bin/main.dart
@@ -2,6 +2,25 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:DartAcademyBootCamp/DartAcademyBootCamp.dart' as DartAcademyBootCamp;
+import 'package:args/args.dart';
+
+ArgResults argResults;
+
+void cmdApp(List<String> arguments) {
+  ArgParser parser = new ArgParser()
+    ..addFlag('line-number', negatable: false, abbr: 'n');
+}
+
+class Point {
+  num x;
+  num y;
+
+  Point(this.x, this.y); // maps directly to properties
+
+  Point.from3D(this.x, this.y, z); // names constructor
+}
+
+const CONSTANT = 10;
 
 void main(List<String> arguments) {
   print("************************ TYPES ************************");
@@ -25,13 +44,27 @@ void main(List<String> arguments) {
   x += 12;
   x++;
   print("${++x} = 20");
+  print("integer division: ${5 ~/ 2}");
+  var a1 = null;
+  a1 ??= 5;
+  var a2 = 1;
+  a2 ??= 5; // assign only if a2 is null
+  print("$a1 != $a2");
+  // cascade operator -> operate on the same object for all the request (does not have to return this)
+  print("string"
+    ..substring(2)
+    ..toString()); // should print the whole string
+  String str = null;
+  print(str?.toUpperCase()); // does not throw nullpointer -> optional .
   print("************************ STRING ************************");
   String d = "text";
   String e = '"text"';
   String f = r'\text';
   String g = "text";
   print("$d $e $f ${g.toUpperCase()[2]}"); // ${} interpolated expression
-  print("************************ CONDITIONS AND BOOLEAN OPERATORS ************************");
+  print(
+      "************************ CONDITIONS AND BOOLEAN OPERATORS ************************");
+  print("${"text" is String}");
   // pretty much the same as in Java
   if ("text" == 'text') {
     print("equal!");
@@ -60,7 +93,7 @@ void main(List<String> arguments) {
   do { // do at least once
     print("${a++}");
   } while (a < 5);
-  for(num i = 0; i < 2; i++){
+  for (num i = 0; i < 2; i++) {
     print("$i");
   }
   print("************************ OTHER ************************");
@@ -69,8 +102,9 @@ void main(List<String> arguments) {
   print("Alternative result is ${1 / 5}");
   print("Some recursion of print: ${print("printing print inside print")}");
   print(5 / 3);
+  cmdApp(arguments);
 }
 
-void printHeader(String header){
+void printHeader(String header) {
   print("************************ $header ************************");
 }
